b6d04eabb111820eef0bbf15515a477ffa5f6d2c
rhama32 <r.hamaoka32915@gmail.com>
2022-11-30 06:41:38 +0000

    Second

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,13 @@
 class BooksController < ApplicationController
-
+before_action :authenticate_user!
+before_action :ensure_correct_user, only: [:edit, :update, :destroy]
   def show
     @book = Book.find(params[:id])
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -20,7 +22,8 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
+    @user = User.find(params[:id])
+    
   end
 
   def update
@@ -32,15 +35,22 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
     @book.destoy
-    redirect_to books_path
+    redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
+  end
+  
+  def ensure_correct_user
+   @book = Book.find(params[:id])
+   unless @book.user == current_user
+   redirect_to books_path
+   end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  get "home/about" => "homes#about"
-
-  resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
-  resources :users, only: [:index, :show, :edit, :update]
+  get 'home/about' => 'homes#about' , as: 'about'
+  resources :users, only: [:show, :index, :edit, :update]
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
