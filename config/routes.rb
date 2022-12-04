3eb1578861147a8f8118be98d1fae692c6c61ddd
rhama32 <r.hamaoka32915@gmail.com>
2022-12-04 09:15:52 +0000

    120402

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about' , as: 'about'
   resources :users, only: [:show, :index, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :destroy] do
-   resources :book_comments, only: [:create]
+   resources :book_comments, only: [:create, :destroy]
    resource :favorites, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
